080e4ba45afda5345b62b30d779e7e3a352f2592
Mohammad Jawwad <mjawwad@stuy.edu>
2019-03-15 00:30:11 -0400

    tested quicksort times

--- Driver.java
+++ Driver.java
@@ -1,6 +1,6 @@
-import java.util.Random;
+import java.util.*;
 public class Driver{
-	public static void fillRandom(int[] data){
+	/*public static void fillRandom(int[] data){
 	    Random r = new Random();
 	    int index = 0;
 	    for(int i = 0; i < data.length; i++){
@@ -87,5 +87,39 @@ public class Driver{
     }
     System.out.println("You have had " + sucesses + " sucesses and " + (tests-sucesses-1) + " failures.");
     System.out.println( (sucesses == tests-1 ? "CONGRATULATIONS!!!" : "Uh Oh")+" Thats " + (sucesses / (tests-1.0)) * 100 + "% sucess rate.");
-  	}
+	}*/
+	public static void main(String[]args){
+  System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
+  int[]MAX_LIST = {1000000000,500,10};
+  for(int MAX : MAX_LIST){
+    for(int size = 31250; size < 2000001; size*=2){
+      long qtime=0;
+      long btime=0;
+      //average of 5 sorts.
+      for(int trial = 0 ; trial <=5; trial++){
+        int []data1 = new int[size];
+        int []data2 = new int[size];
+        for(int i = 0; i < data1.length; i++){
+          data1[i] = (int)(Math.random()*MAX);
+          data2[i] = data1[i];
+        }
+        long t1,t2;
+        t1 = System.currentTimeMillis();
+        Quick.quicksort(data2);
+        t2 = System.currentTimeMillis();
+        qtime += t2 - t1;
+        t1 = System.currentTimeMillis();
+        Arrays.sort(data1);
+        t2 = System.currentTimeMillis();
+        btime+= t2 - t1;
+        if(!Arrays.equals(data1,data2)){
+          System.out.println("FAIL TO SORT!");
+          System.exit(0);
+        }
+      }
+      System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+    }
+    System.out.println();
+  }
+}
 }
